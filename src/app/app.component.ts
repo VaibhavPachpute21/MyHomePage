582a999a6d196687143fef1a08072ff3ced281d0
VaibhavPachpute21 <vpachpute21@gmail.com>
2022-04-23 13:22:45 +0530

    started reusing componets ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,94 +1,64 @@
-import { AfterViewInit, Component, ElementRef, Input, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
-  @ViewChild("textElement")
-  textElement!: ElementRef;
-  @ViewChild("blinkElement")
-  blinkElement!: ElementRef;
-
-  @Input() wordArray: string[] = [
-    "Hi I'm Vaibhav",
-    "I'm Web-Developer...",
-    "I'm Flutter Developer"
-  ];
-  @Input() textColor = "white";
-  @Input() fontSize = "30px";
-  @Input() blinkWidth = "2px";
-  @Input() typingSpeedMilliseconds = 300;
-  @Input() deleteSpeedMilliseconds = 300;
-
-  private i = 0;
-
-  constructor(private renderer: Renderer2) {}
-
-  ngAfterViewInit(): void {
-    this.initVariables();
-    this.typingEffect();
-  }
-
-  private initVariables(): void {
-    this.renderer.setStyle(
-      this.textElement.nativeElement,
-      "color",
-      this.textColor
-    );
-    this.renderer.setStyle(
-      this.textElement.nativeElement,
-      "font-size",
-      this.fontSize
-    );
-    this.renderer.setStyle(this.textElement.nativeElement, "padding", "0.1em");
-
-    this.renderer.setStyle(
-      this.blinkElement.nativeElement,
-      "border-right-width",
-      this.blinkWidth
-    );
-    this.renderer.setStyle(
-      this.blinkElement.nativeElement,
-      "border-right-color",
-      this.textColor
-    );
-    this.renderer.setStyle(
-      this.blinkElement.nativeElement,
-      "font-size",
-      this.fontSize
-    );
-  }
-
-  private typingEffect(): void {
-    const word = this.wordArray[this.i].split("");
-    const loopTyping = () => {
-      if (word.length > 0) {
-        this.textElement.nativeElement.innerHTML += word.shift();
-      } else {
-        this.deletingEffect();
-        return;
-      }
-      setTimeout(loopTyping, this.typingSpeedMilliseconds);
-    };
-    loopTyping();
-  }
-
-  private deletingEffect(): void {
-    const word = this.wordArray[this.i].split("");
-    const loopDeleting = () => {
-      if (word.length > 0) {
-        word.pop();
-        this.textElement.nativeElement.innerHTML = word.join("");
-      } else {
-        this.i = this.wordArray.length > this.i + 1 ? this.i++ : 0;
-
-        this.typingEffect();
-        // return false;
-      }
-      setTimeout(loopDeleting, this.deleteSpeedMilliseconds);
-    };
-    loopDeleting();
-  }
+export class AppComponent {
+  services = [
+    {
+      title: "Website Development",
+      icon:"bi-stack",
+      description: "Web development is the work involved in developing a website for the Internet or an intranet. Web development can range from developing a simple single static page of plain text to complex web applications."
+    },
+    {
+      title: "App Development",
+      icon:"bi-shield",
+      description: "Web development is the work involved in developing a website for the Internet or an intranet. Web development can range from developing a simple single static page of plain text to complex web applications."
+    },
+    {
+      title: "SEO Development",
+      icon:"bi-search",
+      description: "Web development is the work involved in developing a website for the Internet or an intranet. Web development can range from developing a simple single static page of plain text to complex web applications."
+    }
+  ]
+  projects = [
+    {
+      title: "News App",
+      icon:"bi-stack",
+      link:"https://github.com/VaibhavPachpute21/News-Today",
+      description: "This is news app which is created using Flutter and APIs are from newsapi.org. This contains login registration reading news,bookmarking,sharing features etc."
+    },
+    {
+      title: "Study Buddy App",
+      icon:"bi-stack",
+      link:"https://github.com/VaibhavPachpute21/Study-Buddy",
+      description: "This application is to help students to get study mateials.This app provides books,notes on more engineering realted stuff."
+    },
+    {
+      title: "Yo-Yo Test App",
+      icon:"bi-stack",
+      link:"https://github.com/VaibhavPachpute21/News-Today",
+      description: "This app is used for yoyo test i.e fitness test for sports like creicket football. This app uses location features and maps user current location."
+    },
+    {
+      title: "Photography Website With angular",
+      icon:"bi-stack",
+      link:"https://github.com/VaibhavPachpute21/News-Today",
+      description: "This website was made for ViyaFilms.in"
+    },
+    {
+      title: "File converter with Angular",
+      icon:"bi-shield",
+      link:"https://github.com/VaibhavPachpute21/FileConverter",
+      description: "Web development is the work involved in developing a website for the Internet or an intranet. "
+    },
+    {
+      title: "NewsApp Template with angualar",
+      icon:"bi-search",
+      link:"https://github.com/VaibhavPachpute21/newsWebAngular",
+      description: "velopment can range from developing a simple single static page of plain text to complex web applications."
+    }
+  ]
 }
