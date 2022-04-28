3b737ed398025a82c0abfadcd8d517a88804359b
VaibhavPachpute21 <vpachpute21@gmail.com>
2022-04-28 21:28:24 +0530

    started with new idea

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { OldhomeComponent } from './oldhome/oldhome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:OldhomeComponent},
+  {path:"newHome",component:HomeComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
