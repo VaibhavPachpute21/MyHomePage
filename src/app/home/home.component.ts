0b447e071dafbac255f8c5de9eb27b7f0dc89916
VaibhavPachpute21 <vpachpute21@gmail.com>
2022-04-29 19:22:41 +0530

    done with done with new home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  services = [
+    {
+      title: "Website Development",
+      icon:"bi-stack",
+      description: "Web development is the work involved in developing a website for the Internet or an intranet. Web development can range from developing a simple single static page of plain text to complex web applications."
+    },
+    {
+      title: "App Development",
+      icon:"bi-shield",
+      description: "Web development is the work involved in developing a website for the Internet or an intranet. Web development can range from developing a simple single static page of plain text to complex web applications."
+    },
+    {
+      title: "SEO Development",
+      icon:"bi-search",
+      description: "Web development is the work involved in developing a website for the Internet or an intranet. Web development can range from developing a simple single static page of plain text to complex web applications."
+    }
+  ]
+  projects = [
+    {
+      title: "News App",
+      icon:"bi-stack",
+      usedTech:"Flutter,Firebase,News api",
+      link:"https://github.com/VaibhavPachpute21/News-Today",
+      description: "This is news app which is created using Flutter and APIs are from newsapi.org. This contains login registration reading news,bookmarking,sharing features etc."
+    },
+    {
+      title: "Study Buddy App",
+      icon:"bi-stack",
+      usedTech:"Flutter,Firebase,Books",
+      link:"https://github.com/VaibhavPachpute21/Study-Buddy",
+      description: "This application is to help students to get study mateials.This app provides books,notes on more engineering realted stuff."
+    },
+    {
+      title: "Yo-Yo Test App",
+      icon:"bi-stack",
+      usedTech:"Flutter",
+      link:"https://github.com/VaibhavPachpute21/yoyo_test",
+      description: "This app is used for yoyo test i.e fitness test for sports like creicket football. This app uses location features and maps user current location."
+    },
+    {
+      title: "e-Commerce App Template",
+      icon:"bi-stack",
+      usedTech:"Flutter,Firebase,e-Commerce",
+      link:"https://github.com/VaibhavPachpute21/bigBasket",
+      description: "This app is used for yoyo test i.e fitness test for sports like creicket football. This app uses location features and maps user current location."
+    },
+    {
+      title: "Photography Website With angular",
+      icon:"bi-stack",
+      usedTech:"HTML,CSS,Angular,Photgraphy",
+      link:"https://github.com/VaibhavPachpute21/News-Today",
+      description: "This website was made for ViyaFilms.in"
+    },
+    {
+      title: "File converter with Angular",
+      icon:"bi-shield",
+      usedTech:"HTML,CSS,Angular,node",
+      link:"https://github.com/VaibhavPachpute21/FileConverter",
+      description: "Web development is the work involved in developing a website for the Internet or an intranet. "
+    },
+    {
+      title: "NewsApp Template with angualar",
+      icon:"bi-search",
+      usedTech:"HTML,CSS,Angular,news api",
+      link:"https://github.com/VaibhavPachpute21/newsWebAngular",
+      description: "velopment can range from developing a simple single static page of plain text to complex web applications."
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
