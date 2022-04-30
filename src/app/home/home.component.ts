5e8b38c77b580f024bff7a50b8b089891b09dd4a
VaibhavPachpute21 <vpachpute21@gmail.com>
2022-04-30 15:11:46 +0530

    gmail responses started

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -74,9 +76,16 @@ export class HomeComponent implements OnInit {
       description: "velopment can range from developing a simple single static page of plain text to complex web applications."
     }
   ]
-  constructor() { }
+  constructor() { 
+    function heelo(){
+      console.log("hi");
+    };
+  }
 
-  ngOnInit(): void {
+
+  ngOnInit() {
+
+
   }
 
 }
